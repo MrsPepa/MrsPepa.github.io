4273d513f4bc2923f2f0e17b8b308160baa6fd3f
MrsPepa <jose.leon1996@gmail.com>
2018-03-26 18:28:35 -0300

    agregando boton arriba

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,19 @@ $( document ).ready(function(){
     AOS.init();
     $(".button-collapse").sideNav();
     $('.scrollspy').scrollSpy();
+    $('.ir-arriba').click(function(){
+    $('body, html').animate({
+      scrollTop: '0px'
+    }, 300);
+  });
+
+  $(window).scroll(function(){
+    if( $(this).scrollTop() > 0 ){
+      $('.ir-arriba').slideDown(300);
+    } else {
+      $('.ir-arriba').slideUp(300);
+    }
+  });
 })
 
 var Messenger = function(el){
